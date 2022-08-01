6a45749f4eb97e640a173024dba2fb4671975c07
Nivaldo Farias <badivia.f@gmail.com>
2022-08-01 19:26:49 -0300

    chore: run cypress tests

--- front-end/cypress.config.js
+++ front-end/cypress.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 't4q2d1',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
